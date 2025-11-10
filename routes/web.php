dcb8de094a20696dbdc91dfbc9b97efd1a1d5c3d
HyperNeutron <nigel.koremans@gmail.com>
2025-11-10 12:06:27 +0100

    created home page

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Models\Article;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    $popularArticles = Article::orderBy("views", "DESC")->limit(6)->get();
+    return view('home')->with("popularArticles", $popularArticles);
 })->name("home");
 
 Route::get('/test', function () {
