4058bd241b7cb8d91ca1c69b7290ba9fa0c29301
HyperNeutron <nigel.koremans@gmail.com>
2025-11-17 09:38:38 +0100

    added optional options slot

--- resources/views/components/title.blade.php
+++ resources/views/components/title.blade.php
@@ -1,3 +1,8 @@
-<div class="border-b border-gray-300 pb-1.5 mb-3 text-4xl font-semibold">
+<div class="border-b border-gray-300 pb-1.5 mb-2 text-4xl font-semibold flex justify-between">
     {{ $slot }}
+    <div>
+    @if (isset($options))
+        {{ $options }}
+    @endif
+    </div>
 </div>
